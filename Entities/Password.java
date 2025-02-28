6499bbed6eb4f388bef170523701a2409f684a2e
Gustavo Henrique <guazambujinha@gmail.com>
2025-02-28 16:51:52 -0300

    entiti created

---
+++ Entities/Password.java
@@ -0,0 +1,31 @@
+package Entities;
+
+import java.util.UUID;
+
+public class Password {
+    
+    private String passWord;
+
+    public Password(){
+
+    }
+
+    public Password(String passWord){
+        this.passWord = passWord;
+    }
+
+    public String getPassWord() {
+        return passWord;
+    }
+
+    public void setPassWord(String passWord) {
+        this.passWord = passWord;
+    }
+
+    public String generatePassword(){
+
+        String result = UUID.randomUUID().toString().substring(0, 30);
+
+        return result;
+    }
+}
